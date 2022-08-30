f1147c26a1b21879bc412a5215a17daf4b3f31ee
msr8 <live.shashwat@gmail.com>
2022-08-30 22:24:00 +0530

    version 0.0.2

--- src/rcat.py
+++ src/rcat.py
@@ -3,13 +3,17 @@ import rich
 from argparse import ArgumentParser
 import os
 
+
 def init_main():
     parser = ArgumentParser()
     parser.add_argument('file', type=str)
     args = parser.parse_args()
 
     print(args.file)
+    print(os.getcwd())
 
 
-init_main()
+if __name__ == '__main__':
+    init_main()
+
 
